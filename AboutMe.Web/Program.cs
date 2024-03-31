4f2703462aba03a716e41af49e273b39e3790c7b
sergey2martynov <94121806+sergey2martynov@users.noreply.github.com>
2024-03-31 14:16:25 +0400

    Moved identity configuration to extension method

--- AboutMe.Web/Program.cs
+++ AboutMe.Web/Program.cs
@@ -1,49 +1,47 @@
 using AboutMe.HelperExtensions;
 using Infrastructure;
 using Application.AppStart;
-using Core.Entities;
-using Microsoft.AspNetCore.Identity;
 using Core.Mapping;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorPages();
-builder.Services.AddContext(builder.Configuration.GetConnectionString("DefaultConnection"));
-builder.Services.AddServices();
-builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
-
-builder.Services.AddIdentity<User, Role>(options =>
-{
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
-}).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
-
+ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-app.SetupDataBase();
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
-
-//app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
-});
+Configure(app);
 
 app.Run();
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddRazorPages();
+    services.AddContext(builder.Configuration.GetConnectionString("DefaultConnection"));
+    services.AddServices();
+    services.AddAutoMapper(typeof(AutoMapperProfile));
+}
+
+void Configure(WebApplication app)
+{
+    app.SetupDataBase();
+
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        app.UseHsts();
+    }
+
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+    });
+}
--- Infrastructure/Extensions.cs
+++ Infrastructure/Extensions.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure
@@ -8,6 +11,15 @@ namespace Infrastructure
         public static IServiceCollection AddContext(this IServiceCollection services, string connectionString)
         {
             services.AddDbContext<AppDbContext>(options => options.UseMySQL(connectionString));
+
+            services.AddIdentity<User, Role>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
             return services;
         }
     }
